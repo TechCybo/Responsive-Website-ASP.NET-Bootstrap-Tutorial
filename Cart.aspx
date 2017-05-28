ef492196f9874be1787d44943bc52d1652a9f8cc
Shabeer M <mknshabeer@gmail.com>
2017-05-28 19:55:25 +0530

    Part 29 & 30
    Part 29 & 30

--- Cart.aspx
+++ Cart.aspx
@@ -5,40 +5,45 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
     <div style="padding-top: 20px;">
         <div class="col-md-9">
-            <h5 class="proNameViewCart">MY CART (2 Items)</h5>
-            <div class="media" style="border: 1px solid #eaeaec;">
-                <div class="media-left">
-                    <a href="#">
-                        <img width="100px" class="media-object" src="Images/ProductImages/10005/WROGN Men Blue Slim Fit Printed Casual Shirt01.jpg" alt="product">
-                    </a>
-                </div>
-                <div class="media-body">
-                    <h5 class="media-heading proNameViewCart">WROGN Men Blue Slim Fit Printed Casual Shirt</h5>
-                    <p class="proPriceDiscountView">Size : 42</p>
-                    <span class="proPriceView">Rs : 1400</span>
-                    <span class="proOgPriceView">1900</span>
-                    <p>
-                        <asp:Button ID="btnRemoveItem" OnClick="btnRemoveItem_Click" CssClass="removeButton" runat="server" Text="REMOVE" />
-                    </p>
-                </div>
-            </div>
+            <h5 class="proNameViewCart" runat="server" id="h5NoItems"></h5>
+
+            <asp:Repeater ID="rptrCartProducts" runat="server">
+                <ItemTemplate>
+                    <div class="media" style="border: 1px solid #eaeaec;">
+                        <div class="media-left">
+                            <a href="ProductView.aspx?PID=<%#Eval("PID") %>" target="_blank">
+                                <img width="100px" class="media-object" src="Images/ProductImages/<%#Eval("PID") %>/<%#Eval("Name") %><%#Eval("Extention") %>" alt="<%#Eval("Name") %>" onerror="this.src='images/noimage.jpg'">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h5 class="media-heading proNameViewCart"><%#Eval("PName") %></h5>
+                            <p class="proPriceDiscountView">Size : <%#Eval("SizeNamee") %></p>
+                            <span class="proPriceView"><%#Eval("PSelPrice","{0:c}") %></span>
+                            <span class="proOgPriceView"><%#Eval("PPrice","{0:0,00}") %></span>
+                            <p>
+                                <asp:Button CommandArgument='<%#Eval("PID")+"-"+ Eval("SizeIDD")%>' ID="btnRemoveItem" OnClick="btnRemoveItem_Click" CssClass="removeButton" runat="server" Text="REMOVE" />
+                            </p>
+                        </div>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-3" runat="server" id="divPriceDetails">
             <div style="border-bottom: 1px solid #eaeaec;">
                 <h5 class="proNameViewCart">PRICE DETAILS</h5>
                 <div>
                     <label>Cart Total</label>
-                    <span class="pull-right priceGray">1900</span>
+                    <span class="pull-right priceGray" id="spanCartTotal" runat="server"></span>
                 </div>
                 <div>
                     <label>Cart Discount</label>
-                    <span class="pull-right priceGreen">- 500</span>
+                    <span class="pull-right priceGreen" id="spanDiscount" runat="server"></span>
                 </div>
             </div>
             <div>
                 <div class="proPriceView">
                     <label>Total</label>
-                    <span class="pull-right">Rs. 1400</span>
+                    <span class="pull-right" id="spanTotal" runat="server"></span>
                 </div>
                 <div>
                     <asp:Button ID="btnBuyNow" OnClick="btnBuyNow_Click" CssClass="buyNowBtn" runat="server" Text="BUY NOW" />
--- Cart.aspx.cs
+++ Cart.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,12 +12,97 @@ public partial class Cart : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            BindCartProducts();
+        }
+    }
 
+    private void BindCartProducts()
+    {
+        if (Request.Cookies["CartPID"] != null)
+        {
+            string CookieData = Request.Cookies["CartPID"].Value.Split('=')[1];
+            string[] CookieDataArray = CookieData.Split(',');
+            if (CookieDataArray.Length > 0)
+            {
+                h5NoItems.InnerText = "MY CART (" + CookieDataArray.Length + " Items)";
+                DataTable dtBrands = new DataTable();
+                Int64 CartTotal = 0;
+                Int64 Total = 0;
+                for (int i = 0; i < CookieDataArray.Length; i++)
+                {
+                    string PID = CookieDataArray[i].ToString().Split('-')[0];
+                    string SizeID = CookieDataArray[i].ToString().Split('-')[1];
+
+                    String CS = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString1"].ConnectionString;
+                    using (SqlConnection con = new SqlConnection(CS))
+                    {
+                        using (SqlCommand cmd = new SqlCommand("select A.*,dbo.getSizeName(" + SizeID + ") as SizeNamee,"
+                            + SizeID + " as SizeIDD,SizeData.Name,SizeData.Extention from tblProducts A cross apply( select top 1 B.Name,Extention from tblProductImages B where B.PID=A.PID ) SizeData where A.PID="
+                            + PID + "", con))
+                        {
+                            cmd.CommandType = CommandType.Text;
+                            using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                            {
+                                sda.Fill(dtBrands);
+                            }
+
+                        }
+                    }
+                    CartTotal += Convert.ToInt64(dtBrands.Rows[i]["PPrice"]);
+                    Total += Convert.ToInt64(dtBrands.Rows[i]["PSelPrice"]);
+                }
+                rptrCartProducts.DataSource = dtBrands;
+                rptrCartProducts.DataBind();
+                divPriceDetails.Visible = true;
+
+                spanCartTotal.InnerText = CartTotal.ToString();
+                spanTotal.InnerText = "Rs. " + Total.ToString();
+                spanDiscount.InnerText = "- " + (CartTotal - Total).ToString();
+            }
+            else
+            {
+                //TODO Show Empty Cart
+                h5NoItems.InnerText = "Your Shopping Cart is Empty";
+                divPriceDetails.Visible = false;
+            }
+        }
+        else
+        {
+            //TODO Show Empty Cart
+            h5NoItems.InnerText = "Your Shopping Cart is Empty";
+            divPriceDetails.Visible = false;
+
+        }
     }
 
     protected void btnRemoveItem_Click(object sender, EventArgs e)
     {
+        string CookiePID = Request.Cookies["CartPID"].Value.Split('=')[1];
+        Button btn = (Button)(sender);
+        string PIDSIZE = btn.CommandArgument;
 
+        List<String> CookiePIDList = CookiePID.Split(',').Select(i => i.Trim()).Where(i => i != string.Empty).ToList();
+        CookiePIDList.Remove(PIDSIZE);
+        string CookiePIDUpdated = String.Join(",", CookiePIDList.ToArray());
+        if (CookiePIDUpdated == "")
+        {
+            HttpCookie CartProducts = Request.Cookies["CartPID"];
+            CartProducts.Values["CartPID"] = null;
+            CartProducts.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(CartProducts);
+
+        }
+        else
+        {
+            HttpCookie CartProducts = Request.Cookies["CartPID"];
+            CartProducts.Values["CartPID"] = CookiePIDUpdated;
+            CartProducts.Expires = DateTime.Now.AddDays(30);
+            Response.Cookies.Add(CartProducts);
+
+        }
+        Response.Redirect("~/Cart.aspx");
     }
 
     protected void btnBuyNow_Click(object sender, EventArgs e)
